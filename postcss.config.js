c02c57a0db581f5fd7a1684e9a34169bad29be83
Nigel O Toole <pure@purestructure.com>
2023-11-09 16:32:46 +0000

    Social media updates, small refactor of site styles

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,13 @@
 const isProduction = process.env.NODE_ENV === "production";
 const plugins = [
   require('postcss-import'),
-  require('postcss-preset-env')({}),
+  require('postcss-custom-media'),
+  require('postcss-preset-env')({
+    stage: 1
+  }),
 ];
 
 module.exports = {
   map: isProduction ? false : { annotation: true, inline: false },
-  plugins 
+  plugins
 }
